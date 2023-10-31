4186083cbc0136b52c8e1a6ff5c10835c78d3f8f
liujin01 <jin.liu01@yiducloud.cn>
2023-10-31 17:18:05 +0800

    1

---
+++ src/test/java/com/everdata/demo/algo/cache/_146_LRU.java
@@ -0,0 +1,152 @@
+package com.everdata.demo.algo.cache;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * _146_LRU
+ * <pre>
+ * 请你设计并实现一个满足 LRU (最近最少使用) 缓存 约束的数据结构。
+ * 实现 LRUCache 类：
+ * LRUCache(int capacity) 以 正整数 作为容量 capacity 初始化 LRU 缓存
+ * int get(int key) 如果关键字 key 存在于缓存中，则返回关键字的值，否则返回 -1 。
+ * void put(int key, int value) 如果关键字 key 已经存在，则变更其数据值 value ；如果不存在，则向缓存中插入该组 key-value 。如果插入操作导致关键字数量超过 capacity ，则应该 逐出 最久未使用的关键字。
+ * 函数 get 和 put 必须以 O(1) 的平均时间复杂度运行。
+ *
+ * 示例：
+ *
+ * 输入
+ * ["LRUCache", "put", "put", "get", "put", "get", "put", "get", "get", "get"]
+ * [[2], [1, 1], [2, 2], [1], [3, 3], [2], [4, 4], [1], [3], [4]]
+ * 输出
+ * [null, null, null, 1, null, -1, null, -1, 3, 4]
+ *
+ * 解释
+ * LRUCache lRUCache = new LRUCache(2);
+ * lRUCache.put(1, 1); // 缓存是 {1=1}
+ * lRUCache.put(2, 2); // 缓存是 {1=1, 2=2}
+ * lRUCache.get(1);    // 返回 1
+ * lRUCache.put(3, 3); // 该操作会使得关键字 2 作废，缓存是 {1=1, 3=3}
+ * lRUCache.get(2);    // 返回 -1 (未找到)
+ * lRUCache.put(4, 4); // 该操作会使得关键字 1 作废，缓存是 {4=4, 3=3}
+ * lRUCache.get(1);    // 返回 -1 (未找到)
+ * lRUCache.get(3);    // 返回 3
+ * lRUCache.get(4);    // 返回 4
+ * 提示：
+ *
+ * 1 <= capacity <= 3000
+ * 0 <= key <= 10000
+ * 0 <= value <= 105
+ * 最多调用 2 * 105 次 get 和 put
+ * </pre>
+ *
+ * @author liujin
+ * @date 2023/10/20
+ */
+public class _146_LRU {
+    public static void main(String[] args) {
+        LRUCache lruCache = new LRUCache(2);
+        lruCache.put(1, 1);
+        lruCache.put(2, 2);
+        System.out.println(lruCache.get(1));
+        lruCache.put(3, 3);
+        System.out.println(lruCache.get(2));
+        lruCache.put(4, 4);
+        System.out.println(lruCache.get(1));
+        System.out.println(lruCache.get(3));
+        System.out.println(lruCache.get(4));
+    }
+
+
+    //    考察哈希表+双向链表，哈希表存储键与其对应在双向链表中的位置
+    static class LRUCache {
+
+        private Map<Integer, DLinkedNode> cache = new HashMap<>();
+        private int capacity;
+
+        private int size;
+
+        private DLinkedNode head, tail;
+
+        static class DLinkedNode {
+            private int key;
+            private int value;
+            private DLinkedNode prev;
+            private DLinkedNode next;
+
+            public DLinkedNode() {
+            }
+
+            public DLinkedNode(int key, int value) {
+                this.key = key;
+                this.value = value;
+            }
+        }
+
+
+        public LRUCache(int capacity) {
+            size = 0;
+            this.capacity = capacity;
+            head = new DLinkedNode();
+            tail = new DLinkedNode();
+            head.next = tail;
+            tail.prev = head;
+        }
+
+        public int get(int key) {
+            DLinkedNode node = cache.get(key);
+
+            if (node != null) {
+                moveToHead(node);
+                return node.value;
+            } else {
+                return -1;
+            }
+
+        }
+
+        private void moveToHead(DLinkedNode node) {
+            removeNode(node);
+            addToHead(node);
+        }
+
+        private void removeNode(DLinkedNode deleteNode) {
+            deleteNode.prev.next = deleteNode.next;
+            deleteNode.next.prev = deleteNode.prev;
+
+        }
+
+        public void put(int key, int value) {
+            DLinkedNode node = cache.get(key);
+            if (node == null) {
+                DLinkedNode newNode = new DLinkedNode(key, value);
+                cache.put(newNode.key, newNode);
+                addToHead(newNode);
+                size++;
+                if (size > capacity) {
+                    DLinkedNode lastNode = removeLastNode();
+                    cache.remove(lastNode.key);
+                    size--;
+                }
+            } else {
+                node.value = value;
+                moveToHead(node);
+            }
+        }
+
+        private DLinkedNode removeLastNode() {
+            DLinkedNode lastNode = tail.prev;
+            removeNode(lastNode);
+            return lastNode;
+        }
+
+
+        private void addToHead(DLinkedNode node) {
+            node.prev = head;
+            node.next = head.next;
+            head.next.prev = node;
+            head.next = node;
+        }
+    }
+
+}
